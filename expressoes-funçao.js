0874c9f0efeabd176d8d5cfb3bb7573407ddcd32
Allan Teran <allteran655@gmail.com>
2021-08-30 23:43:51 -0300

    function

--- expressoes-funçao.js
+++ expressoes-funçao.js
@@ -1,3 +1,13 @@
-const soma = function(num1, num2) {return num1 + num2}
+// expressoes de função
 
-console.log(soma(1, 3))
+//const soma = function(num1, num2) {return num1 + num2}
+
+//console.log(soma(1, 1))
+
+// difença entre a forma normal e a forma de expressão
+
+console.log(apresentar())
+
+function apresentar(){
+    return "olá!"
+}
